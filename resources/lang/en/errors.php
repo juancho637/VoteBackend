4bf7e2ecca6eddac1d212eaf58e5a4ef53a3a845
Juan David Garcia Reyes <juan_dav.garcia@scriptf.com>
2019-03-26 10:18:06 -0500

    archivos para las traducciones

---
+++ resources/lang/en/errors.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'does not exist any instance' => 'Does not exist any instance of :model whit the given id',
+    'unexpected error' => 'Unexpected error. Try later',
+    'Method Not Allowed' => 'Method Not Allowed',
+];
---
+++ resources/lang/es/errors.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'does not exist any instance' => 'No existe ninguna instancia de :model con este id',
+    'unexpected error' => 'Error inesperado, intenta más tarde',
+    'Method Not Allowed' => 'Método no permitido',
+];
